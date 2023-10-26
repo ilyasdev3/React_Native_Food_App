1ba5c1f894110a67ee1c86278914bf54ad07fde0
ilyasdev3 <ilyassanjrani708@gmail.com>
2023-10-26 19:08:44 +0500

    Add provider in app component

--- App.tsx
+++ App.tsx
@@ -1,7 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import NavigationProvider from "./navigations/NavigationProvider";
+import { Providers } from "./redux-store/Provider";
+import { FoodProvider } from "./context/foodContent";
 
 export default function App() {
-  return <NavigationProvider />;
+  return (
+    <FoodProvider>
+      <Providers>
+        <NavigationProvider />
+      </Providers>
+    </FoodProvider>
+  );
 }
